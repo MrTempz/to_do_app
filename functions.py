8489d6b9b209fc04fd8692ecc83b74c97ab62dc0
Adam <adamtemplin92@gmail.com>
2022-11-26 14:10:23 +0100

    move function logic to functions.py as dictionary

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ MESSAGES = {'input':'Type add, show, edit, complete or exit: ',
     'edit_choice': 'Number of todo to edit: ',
     'edit_new': 'Enter a new todo: ',
     'complete': 'Number of todo to complete: '}
+
 FILEPATH = 'App1\\todos.txt'
 
 
@@ -16,7 +17,42 @@ def read_file(filepath=FILEPATH):
     with open(filepath, 'r') as f:
         todos_list = f.readlines()
         return todos_list
-    
+
+def add(todos, user_params):
+    todo = user_params
+    todos.append(todo + '\n')
+    write_file(todos, FILEPATH)
+
+def validate_todo_no(todos, todo_no, action):
+    try:
+        todo_no = int(todo_no) - 1
+        return todo_no
+    except ValueError as e:
+        print('Number expected after "{action}"')
+        return None
+    except IndexError as e:
+        print(f"Could not {action} todo No. {todo_no+1}, max possible todo No. is {len(todos)}")
+        return None
+
+def edit(todos, user_params):
+    todo_no, new_todo = user_params.split(' ', 1)
+    todo_no = validate_todo_no(todos, todo_no, 'edit')
+    if todo_no:
+        todos[todo_no] = new_todo + '\n'
+        write_file(todos, FILEPATH)
+
+def complete(todos, user_params):
+    todo_no = validate_todo_no(todos, user_params, 'complete')
+    if todo_no:
+        todos.pop(todo_no)
+        write_file(todos, FILEPATH)
+
+def show(todos, user_params):
+    for index, todo in enumerate(todos):
+        print(f'{index + 1} - {todo.capitalize().strip()}')
+
+ACTIONS = {'add':add, 'edit':edit, 'complete':complete, 'show':show}
+
 if __name__ == "__main__":
     print("This is functions.py")
     print(read_file())
--- prototype2.py
+++ prototype2.py
@@ -1,49 +1,25 @@
-from functions import MESSAGES, FILEPATH, read_file, write_file
+import functions
 import time
 
 now = time.strftime("%b %d, %Y %H:%M:%S")
 print(f"It's {now}")
 
-todos = read_file(FILEPATH)
+
 
 while True:
-    user_action = input(MESSAGES['input']).lower()
-
-    if user_action.startswith('add'):
-        todo = user_action[4:]
-        todos.append(todo + '\n')
-        write_file(todos, FILEPATH)
-
-    elif user_action.startswith('edit'):
-        try:
-            todo_no = int(user_action[5:]) - 1
-            new_todo = input(MESSAGES['edit_new']) + '\n'
-            todos[todo_no] = new_todo
-            write_file(todos, FILEPATH)
-        except ValueError as e:
-            print('Number expected after "edit"')
-            continue
-        except IndexError as e:
-            print(f"Could not edit todo No. {todo_no+1}, max possible todo No. is {len(todos)}")
-            continue
-        
-    elif user_action.startswith('complete'):
-        try:
-            todo_no = int(user_action[9:]) - 1
-            todos.pop(todo_no)
-            write_file(todos, FILEPATH)
-        except ValueError as e:
-            print('Number expected after "complete"')
-            continue
-        except IndexError as e:
-            print(f"Could not complete todo No. {todo_no+1}, max possible todo No. is {len(todos)}")
-            continue
-
-    elif user_action.startswith('show'):
-        for index, todo in enumerate(todos):
-            print(f'{index + 1} - {todo.capitalize().strip()}')
+    user_action = input(functions.MESSAGES['input']).lower()
+    user_choice = user_action.split(' ',1)[0]
+    if user_choice!= user_action:
+        user_params = user_action.split(' ',1)[1]
+    else:
+        user_params = None
     
-    elif user_action.startswith('exit'):
+    if user_choice in functions.ACTIONS.keys():
+        action = functions.ACTIONS[user_choice]
+        todos = functions.read_file(functions.FILEPATH)
+        action(todos, user_params)
+    
+    elif user_choice == 'exit':
         break
 
 print('Bye!')
