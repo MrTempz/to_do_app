291461fa1c66beb4e3d78b46853cd0cb9bf96bca
Adam <adamtemplin92@gmail.com>
2022-11-28 11:23:38 +0100

    Day-17 handle closing and adding

--- gui_prototype.py
+++ gui_prototype.py
@@ -2,10 +2,24 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter a to-do")
+layout = [[label]]
+
+input_box = sg.InputText(tooltip="Enter a to-do", key="todo")
 add_button = sg.Button("Add")
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
+layout.append([input_box, add_button])
 
+window = sg.Window('My To-Do App', layout=layout, font=('Helvatica', 20))
 
-window.read()
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add':
+            todos = functions.get_todos(filepath=functions.FILEPATH)
+            new_todo = values['todo']
+            functions.add(todos, new_todo)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
