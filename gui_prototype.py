b3f1ff001ad664147ba74055080385ee5de85132
Adam <adamtemplin92@gmail.com>
2022-11-28 13:09:58 +0100

    Day17 - add Edit option and display todos

--- gui_prototype.py
+++ gui_prototype.py
@@ -1,12 +1,16 @@
 import functions
 import PySimpleGUI as sg
 
-label = sg.Text("Type in a to-do")
+label = sg.Text('Type in a to-do')
 layout = [[label]]
 
-input_box = sg.InputText(tooltip="Enter a to-do", key="todo")
-add_button = sg.Button("Add")
+input_box = sg.InputText(tooltip='Enter a to-do', key='todo')
+add_button = sg.Button('Add')
 layout.append([input_box, add_button])
+
+todos_box = sg.Listbox(values=functions.get_todos(), key='todos', enable_events=True, size=[45,10])
+edit_button = sg.Button('Edit')
+layout.append([todos_box, edit_button])
 
 window = sg.Window('My To-Do App', layout=layout, font=('Helvatica', 20))
 
@@ -19,7 +23,18 @@ while True:
             todos = functions.get_todos(filepath=functions.FILEPATH)
             new_todo = values['todo']
             functions.add(todos, new_todo)
+            window['todos'].update(values=functions.get_todos())
+        case 'Edit':
+            todos = functions.get_todos(filepath=functions.FILEPATH)
+            todo = values['todos'][0]
+            todo_no = todos.index(todo)
+            new_todo = values['todo']
+            functions.edit(todos, todo_no, new_todo)
+            window['todos'].update(values=functions.get_todos())
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
+    
 
 window.close()
